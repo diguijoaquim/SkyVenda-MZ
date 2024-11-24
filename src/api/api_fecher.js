7d3f993bd9d3670ff7ecfa3b9c0ebed8ab426468
Ghost404 <diguijoaquim@users.noreply.github.com>
2024-11-24 08:04:21 +0200

    n

--- src/api/api_fecher.js
+++ src/api/api_fecher.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Configuração padrão
 const api = axios.create({
-    baseURL: 'http://192.168.1.62:8000', // Altere para sua URL base
+    baseURL: 'https://skyvendamz.onreder.com', // Altere para sua URL base
   });
   
 export default api;
