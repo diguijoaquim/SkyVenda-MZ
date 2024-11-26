659758cd3ec489a3105e4f9dd042682c62b27241
Ghost404 <diguijoaquim@users.noreply.github.com>
2024-11-26 15:13:54 +0200

    ooo

--- src/api/api_fecher.js
+++ src/api/api_fecher.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Configuração padrão
 const api = axios.create({
-    baseURL: 'http://skyvendamz.up.railway.app', // Altere para sua URL base
+    baseURL: 'https://skyvendamz.up.railway.app', // Altere para sua URL base
   });
   
 export default api;
